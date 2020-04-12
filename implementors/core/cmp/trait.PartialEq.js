a60b9db80116178ef18fc677c1651de6ebbd3e46
GopherJ <GopherJ@users.noreply.github.com>
2020-04-12 08:19:19 +0000

    deploy: 51201942ae59b67a9a02c1216f1b5ada6c7f96c9

---
+++ implementors/core/cmp/trait.PartialEq.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["casbin"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a>&lt;<a class=\"enum\" href=\"casbin/enum.EffectKind.html\" title=\"enum casbin::EffectKind\">EffectKind</a>&gt; for <a class=\"enum\" href=\"casbin/enum.EffectKind.html\" title=\"enum casbin::EffectKind\">EffectKind</a>","synthetic":false,"types":["casbin::effector::EffectKind"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["casbin"] = {"name":"","dirs":[{"name":"adapter","files":["file_adapter.rs","memory_adapter.rs","mod.rs"]},{"name":"cache","files":["default_cache.rs","mod.rs"]},{"name":"model","files":["assertion.rs","default_model.rs","function_map.rs","mod.rs"]},{"name":"rbac","files":["default_role_manager.rs","mod.rs","role_manager.rs"]}],"files":["cached_api.rs","cached_enforcer.rs","config.rs","convert.rs","core_api.rs","effector.rs","emitter.rs","enforcer.rs","error.rs","internal_api.rs","lib.rs","management_api.rs","prelude.rs","rbac_api.rs","util.rs","watcher.rs"]};
+createSourceSidebar();

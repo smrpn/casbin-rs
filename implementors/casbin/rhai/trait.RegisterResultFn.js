6fadf31f89c6c1cb2739e6a64f43ca5600e52b93
GopherJ <GopherJ@users.noreply.github.com>
2020-07-20 09:36:36 +0000

    deploy: ca7a25133ef03a61dcab4a763d684fcc37f1aa90

---
+++ implementors/casbin/rhai/trait.RegisterResultFn.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["casbin"] = [];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
